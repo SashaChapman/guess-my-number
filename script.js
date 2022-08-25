8fef6507512f62944347ce9b7d058e7cf32f5678
Sasha Chapman <sashachapman@Sashas-MacBook-Pro.local>
2022-08-25 13:46:41 +0100

    Finished game

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 'use strict';
 
-/*console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct number';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-console.log((document.querySelector('.guess').value = 23));
-document.querySelector('.guess').value;*/
-
-//Manipulating CSS styles
-//Changing the background to green when the correct number is picked. The dash notation on CSS when there is two words doesnt work for javaScript so it adopts the camel case instead (background color)
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -22,30 +13,41 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guess, typeof guess);
   //When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number!';
+    displayMessage('No Number!');
     //When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct number!';
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    //When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Higher!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lose!';
-      document.querySelector('.score').textContent = 0;
+    displayMessage('Correct number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347'; //Background color when win
+    document.querySelector('.number').style.width = '30rem'; //Width when win
+    //Check highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
-    //When guess is too High
-  } else if (guess > secretNumber) {
+  }
+  //When guess is wrong
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Lower!';
+      displayMessage(guess < secretNumber ? 'Higher!' : 'Lower!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lose!';
+      displayMessage('You lose!');
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+//Resetting game
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = secretNumber;
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  displayMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
 });
